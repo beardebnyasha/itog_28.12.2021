3f5d2c71943bdb3070ed0d4f55d7c139748f24c5
beardebnyasha <puzik1995@mail.ru>
2021-12-28 21:36:50 +0300

    last step creat new array

--- itog/Program.cs
+++ itog/Program.cs
@@ -10,10 +10,40 @@ return array;
 
 void PrintArray(int[] array)
 {
-    foreach (int el in array)
+    foreach (int index in array)
     {
-        System.Console.Write(el + " ");
+        System.Console.Write(index + " ");
     }
 }
 
-PrintArray(CreatNewArray(10));
+
+int[] NewArray(int[] array)
+{
+    int NewArrayLenght = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 0)
+        {
+            NewArrayLenght++;
+        }
+    }
+    int[] NewArray = new int[NewArrayLenght];
+    int number = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 0)
+        {
+            NewArray[number] = array[i];
+            number++;
+        }
+    }
+    return NewArray;
+}
+int[] Resultarray;
+
+System.Console.Write("Первоначальный массив: ");
+Resultarray = CreatNewArray(10);
+PrintArray(Resultarray);
+System.Console.WriteLine(" ");
+System.Console.Write("Результирующий массив: ");
+PrintArray(NewArray(Resultarray));
