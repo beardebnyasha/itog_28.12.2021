af86257a1ba3627c34907a5e22a2713b01523c26
beardebnyasha <puzik1995@mail.ru>
2021-12-28 21:08:08 +0300

    first step create array

---
+++ itog/Program.cs
@@ -0,0 +1,9 @@
+﻿int[] CreatNewArray (int lenght, int firstNum, int lustNum)
+{
+    int[] array = new int[lenght];
+    for (int i = 0; i < lenght; i++)
+    {
+        array[i] = new Random().Next(firstNum, lustNum);
+    }
+return array;
+}
