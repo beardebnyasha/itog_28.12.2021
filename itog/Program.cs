3cec3f7de82ffa4341ed2b20d29524d79dd6027e
beardebnyasha <puzik1995@mail.ru>
2021-12-28 21:16:32 +0300

    second step print array

--- itog/Program.cs
+++ itog/Program.cs
@@ -1,9 +1,19 @@
-﻿int[] CreatNewArray (int lenght, int firstNum, int lustNum)
+﻿int[] CreatNewArray (int lenght)
 {
     int[] array = new int[lenght];
     for (int i = 0; i < lenght; i++)
     {
-        array[i] = new Random().Next(firstNum, lustNum);
+        array[i] = new Random().Next(1, 100);
     }
 return array;
 }
+
+void PrintArray(int[] array)
+{
+    foreach (int el in array)
+    {
+        System.Console.Write(el + " ");
+    }
+}
+
+PrintArray(CreatNewArray(10));
